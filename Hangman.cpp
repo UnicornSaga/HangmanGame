a080a06ba3de6246bc2c24af24d59f573ffb8e09
UnicornSaga <38356503+UnicornSaga@users.noreply.github.com>
2021-04-05 22:52:17 +0700

    Update Hangman.cpp

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+#include <thread>
+#include <chrono>
 using namespace std;
 
 string generateWord(); //sinh ra 1 từ random để ng chơi đoán
@@ -12,7 +15,6 @@ void welcomeHeadline(int& charCount); //in ra màn hình 1 welcome statement
 void printGuessLetter(vector<char>& checkGuess); //in ra những từ đã được ng chơi đoán
 void clearScreen();
 void renderGame();
-
 
 int main()
 {
@@ -54,17 +56,14 @@ void generateWord(string& word, string& hint)
 
 
 
-char inputChar(vector<char>& checkGuess)
+string inputChar(vector<char>& checkGuess, string& genWord)
 {
     string guess;
     int check = 0;
     do{
         cout << "Your guess: ";
         cin >> guess;
-        if (guess.length() > 1){
-            cout << "Please enter only 1 character. Try again." << endl;
-        }
-        else if (char(tolower(guess[0])) < 97 or char(tolower(guess[0])) > 122){
+        if (char(tolower(guess[0])) < 97 || char(tolower(guess[0])) > 122){
             cout << "This is not a character. Try again." << endl;
         }
         else{
@@ -76,9 +75,9 @@ char inputChar(vector<char>& checkGuess)
                 }
             }
         }
-    } while(guess.length() > 1 or (char(tolower(guess[0])) < 97 or char(tolower(guess[0])) > 122));
+    } while((char(tolower(guess[0])) < 97 || char(tolower(guess[0])) > 122));
     if (check == 0) checkGuess.push_back(char(tolower(guess[0])));
-    return char(tolower(guess[0]));
+    return guess;
 }
 
 
@@ -177,12 +176,17 @@ void printGuessLetter(vector<char>& checkGuess)
 
 void clearScreen()
 {
-    for (int i = 0; i < 30 ; i++) cout << endl;
+    #ifdef _WIN32
+        system("cls");
+    #elif __linux__
+        system("clear");
+    #endif // _WIN32
 }
 
 void renderGame()
 {
     int point = 0;
+    int check = 0;
     string genWord, hint;
     generateWord(genWord, hint);
     int wordLen = genWord.length(); //lấy độ dài từ đc generate
@@ -205,7 +209,7 @@ void renderGame()
         //Nếu ppoint lớn hơn 5 thì hỏi ng chơi có cần hint không
         bool checkHint = false;
         char tmp;
-        if (point >= 5 and insertHint == 0){
+        if (point >= 5 && insertHint == 0){
             cout << "Do you want a hint?(Y|N): ";
             cin >> tmp;
             tmp = char(tolower(tmp));
@@ -219,14 +223,23 @@ void renderGame()
         }
 
         cout << endl << str << endl;
-        char guess = inputChar(checkGuess);
-        if (genWord.find_first_of(guess) == string::npos){
+        string guess = inputChar(checkGuess, genWord);
+        if (genWord.find_first_of(guess[0]) == string::npos){
             point++;
+        }
+        else if (guess == genWord){
+            check++;
+            break;
+        }
+        else if (guess.length() > 1 && guess != genWord){
+            cout << "Sorry but you have lost :((" << endl;
+            cout << "The word is: " << genWord << endl;
+            break;
         }
         else{
             for (int i = 0; i < genWord.length(); i++){
-                if (guess == genWord[i]){
-                    str[i] = guess;
+                if (guess[0] == genWord[i]){
+                    str[i] = guess[0];
                     wordLen--;
                 }
             }
@@ -234,8 +247,13 @@ void renderGame()
         clearScreen();
     }
         //Nếu đã đoán quá 7 lần mà vẫn chưa đoán hết các từ thì thua
-    if (point > 7 and wordLen != 0){
-        cout << "Sorry but you have lost cunt!!" << endl;
-        cout << "The correct word is: " << genWord << endl;
+    if (point > 7 && wordLen != 0){
+        cout << "Sorry but you have lost :((" << endl;
+        cout << "The word is: " << genWord << endl;
+    }
+    if (check != 0){
+        cout << "Congrats, You have won!!!" << endl;
+        cout << "The word is: " << genWord << endl;
     }
 }
+        
